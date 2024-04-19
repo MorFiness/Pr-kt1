235c81b3d052a02736fa1239e40cfca12e8dcb1b
MorFiness <MorFiness@DESKTOP-JO29832>
2024-04-19 23:40:26 +0700

    Добавьте файлы проекта.

---
+++ Prаkt1/Prаkt1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+double n, n2, n3 = 0;//Объявление переменных
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Вводите число n""\n";//Вывод на консоли
+    cin >> n;//Ввод числа n
+    n2 = n * (n + 1);//расчет по формуле 2
+    for (int j = 1; j < n; j++) {
+        n3 += 2 * j;//расчет по формуле 1
+    }
+    if (n2 = n3)//сравнение 
+        cout << "числа равны" << endl;//вывод
+    else
+        cout << "Числа не равны" << endl;//вывод
+}
